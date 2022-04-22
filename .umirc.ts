fd1d1349e630d65db582f17cc047ed278af80553
huxinfeng <huxinfeng@laiye.com>
2022-04-22 18:26:40 +0800

    fix: 解决打包后，html引用不到js和css的问题

--- .umirc.ts
+++ .umirc.ts
@@ -5,7 +5,17 @@ export default defineConfig({
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  /**
+   * 打包输出路径
+   */
   outputPath: 'docs-dist',
+  /**
+   * 站点模式
+   */
   mode: 'site',
+  /**
+   * 解决html引用不到 js 和 css 的问题
+   */
+  publicPath: './',
   // more config: https://d.umijs.org/config
 });
